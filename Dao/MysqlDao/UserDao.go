58e5a3f7db97428287f394a6cc8bba2109c664ed
LZP <3331702096@qq.com>
2023-08-13 15:13:39 +0800

    feat: 实现了用户注册，用户登录，获取用户信息接口

--- Dao/MysqlDao/UserDao.go
+++ Dao/MysqlDao/UserDao.go
@@ -1,1 +1,118 @@
 package MysqlDao
+
+import (
+	"douyin/Entity/RequestEntity"
+	"douyin/Entity/TableEntity"
+	"douyin/Log"
+	"douyin/Util"
+	"gorm.io/gorm"
+)
+
+type UserDao interface {
+
+	// AddUser 添加用户，同时操作user_infos和user_account_infos表，并且返回userId
+	AddUser(user RequestEntity.User) (int64, error)
+
+	// CountUserByUsername 根据用户名获取user的数量
+	CountUserByUsername(username string) (int64, error)
+
+	// GetUserById 根据id获取user
+	GetUserById(id int64) (*RequestEntity.User, error)
+
+	// GetUserIdByUsernameANDPassword 根据用户名和密码获取userId
+	GetUserIdByUsernameANDPassword(id int64) (int64, error)
+}
+type UserDaoImpl struct {
+}
+
+func (u *UserDaoImpl) AddUser(rUser RequestEntity.RegisterRequest) (int64, error) {
+
+	//根据雪花算法生成id
+	userId, _ := Util.MakeUid(Util.Snowflake.DataCenterId, Util.Snowflake.MachineId)
+
+	//MD5加密密码
+	password := Util.CalcMD5(rUser.Password)
+
+	var userAccount = &TableEntity.UserAccountInfo{
+		UserID:   userId,
+		Password: password,
+		Username: rUser.Username,
+	}
+
+	var user = &TableEntity.UserInfo{
+		FavoriteCount:  0,
+		FollowCount:    0,
+		FollowerCount:  0,
+		ID:             userId,
+		IsFollow:       false,
+		Name:           rUser.Username,
+		Signature:      "大家好，我是" + rUser.Username,
+		TotalFavorited: 0,
+		WorkCount:      0,
+	}
+
+	//开启gorm事务
+	err := mysqldb.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(userAccount).Error; err != nil {
+			// 返回任何错误都会回滚事务
+			return err
+		}
+		if err := tx.Create(user).Error; err != nil {
+			return err
+		}
+		// 返回 nil 提交事务
+		return nil
+	})
+	if err != nil {
+		return 0, err
+	}
+	return userId, nil
+}
+
+func (u *UserDaoImpl) CountUserByUsername(username string) (int64, error) {
+
+	var num int64
+
+	err := mysqldb.Model(&TableEntity.UserAccountInfo{}).Where("username =?", username).Count(&num).Error
+
+	if err != nil {
+		Log.ErrorLogWithoutPanic("CountUserByUsername出错", err)
+		return -1, err
+	}
+
+	return num, nil
+}
+
+func (u *UserDaoImpl) GetUserById(id int64) (*RequestEntity.User, error) {
+
+	//根据id获取user
+	var user = &RequestEntity.User{}
+
+	err := mysqldb.Model(&TableEntity.UserInfo{}).Where("id=?", id).First(&user).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *UserDaoImpl) GetUserIdByUsernameANDPassword(username, password string) (*int64, error) {
+
+	//根据UsernameANDPassword获取userId
+	var users []TableEntity.UserAccountInfo
+
+	err := mysqldb.Model(&TableEntity.UserAccountInfo{}).
+		Where("username = ? AND password = ?", username, password).
+		Find(&users).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	//没有查询到记录
+	if len(users) == 0 {
+		return nil, nil
+	}
+
+	return &users[0].UserID, nil
+}
--- Service/User.go
+++ Service/User.go
@@ -1,26 +1,129 @@
 package Service
 
 import (
+	"douyin/Dao/MysqlDao"
 	"douyin/Entity/RequestEntity"
+	"douyin/Log"
+	"douyin/Util"
+	"strconv"
 )
 
+var userDaoImpl *MysqlDao.UserDaoImpl
+
 func UserRegisterProcess(request RequestEntity.RegisterRequest) RequestEntity.RegisterBack {
-	var (
-		registerBack RequestEntity.RegisterBack
-	)
-	return registerBack
+	//var (
+	//	registerBack RequestEntity.RegisterBack
+	//)
+	Log.NormalLog("UserRegisterProcess", nil)
+
+	//获取使用该用户名的账号数量
+	num, err := userDaoImpl.CountUserByUsername(request.Username)
+	if err != nil {
+		Log.ErrorLogWithoutPanic("register failed", err)
+		return RequestEntity.RegisterBack{
+			StatusCode: 1,
+			StatusMsg:  "register failed",
+		}
+	}
+
+	//不为0，已经存在使用该用户名的用户，不允许注册
+	if num != 0 {
+		Log.ErrorLogWithoutPanic("user already registered", nil)
+		return RequestEntity.RegisterBack{
+			StatusCode: 1,
+			StatusMsg:  "user already registered",
+		}
+	}
+
+	//允许注册，进行持久化储存
+	userId, err := userDaoImpl.AddUser(request)
+	if err != nil {
+		Log.ErrorLogWithoutPanic("register failed", err)
+		return RequestEntity.RegisterBack{
+			StatusCode: 1,
+			StatusMsg:  "register failed",
+		}
+	}
+
+	//生成token，并返回给客户端
+	token := Util.MakeToken(userId)
+	Log.NormalLog("user register success:"+strconv.FormatInt(userId, 10), nil)
+	return RequestEntity.RegisterBack{
+		StatusCode: 0,
+		StatusMsg:  "register success",
+		Token:      token,
+		UserID:     userId,
+	}
 }
 
 func UserLoginProcess(request RequestEntity.LoginRequest) RequestEntity.LoginBack {
-	var (
-		loginBack RequestEntity.LoginBack
-	)
-	return loginBack
+	//var (
+	//	loginBack RequestEntity.LoginBack
+	//)
+
+	//根据用户名和密码查询用户并返回useId
+	Log.NormalLog("UserLoginProcess", nil)
+	userId, err := userDaoImpl.GetUserIdByUsernameANDPassword(request.Username, Util.CalcMD5(request.Password))
+	if err != nil || userId == nil {
+		statusMsg := "username or password is incorrect"
+		Log.NormalLog(statusMsg, err)
+		return RequestEntity.LoginBack{
+			StatusCode: 1,
+			StatusMsg:  &statusMsg,
+		}
+	}
+
+	//生成token，并返回给客户端
+	statusMsg := "login success"
+	token := Util.MakeToken(*userId)
+	Log.NormalLog("user login success:"+strconv.FormatInt(*userId, 10), nil)
+	return RequestEntity.LoginBack{
+		StatusCode: 0,
+		StatusMsg:  &statusMsg,
+		Token:      &token,
+		UserID:     userId,
+	}
 }
 
 func UserInfoProcess(request RequestEntity.UserInfoRequest) RequestEntity.UserInfoBack {
-	var (
-		userInfoBack RequestEntity.UserInfoBack
-	)
+	//var (
+	//	userInfoBack RequestEntity.UserInfoBack
+	//)
+
+	Log.NormalLog("UserInfoProcess", nil)
+
+	//解析token
+	userId, err := Util.ParserToken(request.Token)
+
+	//token解析错误，返回
+	if err != nil {
+		statusMsg := "token parse error, please log in and try again"
+		Log.ErrorLogWithoutPanic(statusMsg, err)
+		return RequestEntity.UserInfoBack{
+			StatusCode: 1,
+			StatusMsg:  &statusMsg,
+			User:       nil,
+		}
+	}
+
+	//获取用户信息
+	user, err := userDaoImpl.GetUserById(userId)
+	if err != nil {
+		statusMsg := "get user info failed"
+		Log.ErrorLogWithoutPanic(statusMsg, err)
+		return RequestEntity.UserInfoBack{
+			StatusCode: 1,
+			StatusMsg:  &statusMsg,
+			User:       nil,
+		}
+	}
+
+	//返回用户信息
+	statusMsg := "get user info success"
+	userInfoBack := RequestEntity.UserInfoBack{
+		StatusCode: 0,
+		StatusMsg:  &statusMsg,
+		User:       user,
+	}
 	return userInfoBack
 }
